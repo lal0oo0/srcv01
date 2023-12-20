0adc1210c527c4338d7aabdf829ac3701f11d7ec
anyt1 <anasesmagarcia@gmail.com>
2023-12-20 10:25:14 -0600

    uefurejfur

--- Vista/srcv_historial1.php
+++ Vista/srcv_historial1.php
@@ -41,38 +41,36 @@
   ?>
  
  <header>
-  <nav class="navbar navbar-dark  fixed-top navbar-custom  " >
+  <nav class="navbar navbar-dark  fixed-top navbar-custom" >
   <div class="container-fluid">
     <a class="navbar-brand" href="#"><img src="../imagenes/logo_it.png" width="60px"> SRCV Historial</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="../imagenes/menu3.png" width="40px">
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Historial</a></li>
-            <li><a class="dropdown-item" href="#">Cerrar sesion</a></li>
-            <li><a class="dropdown-item" href="#">Cerrar Aplicacion</a></li>
-          </ul>
-        </li>
-      </ul>
-      <a class="navbar-brand" href="#"><img src="../imagenes/logo_it.png" width="60px"> SRCV Historial de Visitas</a>
-      <div class="container">
-        <div class="row">
-            <div class="col-md-7">
-            </div>
-            <div class="col-md-5">
-      <form class="d-flex ">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <div class="offcanvas offcanvas-end navbar-custom" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title tituloM" id="offcanvasDarkNavbarLabel">Menu</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" href="http://localhost/srcv01/Vista/srcv_registro.php">Registro</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Cerrar Sesion</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Cerrar Aplicacion</a>
+          </li>
+        </ul>
+        </ul>
+        </ul>
 
-      </div>
-      </div>
+        <form class="d-flex mt-3" role="search">
+          <input class="form-control me-2" type="Buscar" placeholder="Buscar" aria-label="Buscar">
+          <button class="btn btn-success" type="submit">Buscar</button>
+        </form>
       </div>
     </div>
   </div>
