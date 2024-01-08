ae88870f165582eb328aec785216137e479860d5
anyt1 <anasesmagarcia@gmail.com>
2024-01-08 12:32:41 -0600

    jolkkj

--- Vista/vista_registro_categorias.php
+++ Vista/vista_registro_categorias.php
@@ -101,7 +101,41 @@ session_start();
 <br>
 <h3 class="text-center">LISTA DE CATEGORÍAS</h3>
 <div class="mb-5"></div> <!--Salto de linea-->
+<div class="container caja">
+  <div class="row">
+    <div class="col-md-12">
+        <label for="Nombre" class="col-form-label">Registro de una nueva categoría</label>
+    </div>
+  </div>
+</div>
 
+<div class="container caja2">
+  <div class="row">
+    <div class="col-md-12">
+      <form action="../Controlador/controlador_registro_categoria.php" class="formulario" method="post">
+        <div class="row g-3 align-items-center">
+          <div class="col-md-2">
+          <label for="Nombre" class="col-form-label">Nombre de la categoría:</label>
+          </div>
+          <div class="col-md-6">
+          <input type="text" class="form-control" name="Nombre" placeholder="Ingresa el nombre de la categoría" aria-label="Nombre" aria-describedby="basic-addon1" required>
+          </div>
+          <div class="col-md-3">
+          <select class="form-select" name="Categoria" aria-label="Default select example">
+            <option selected>Selecciona la categoría</option>
+            <option value="Empresa">Empresa</option>
+            <option value="Asunto">Asunto</option>
+          </select>
+          </div>
+          <div class="col-md-1">
+          <button type="submit" class="btn btn-danger">AGREGAR</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<div class="mb-5"></div> <!--Salto de linea-->
 <div class="container">
   <div class="row">
     <div class="col">
@@ -146,41 +180,6 @@ session_start();
 </div>
 <br>
 
-<div class="mb-5"></div> <!--Salto de linea-->
-<div class="container caja">
-  <div class="row">
-    <div class="col-md-12">
-        <label for="Nombre" class="col-form-label">Registro de una nueva categoría</label>
-    </div>
-  </div>
-</div>
-
-<div class="container caja2">
-  <div class="row">
-    <div class="col-md-12">
-      <form action="../Controlador/controlador_registro_categoria.php" class="formulario" method="post">
-        <div class="row g-3 align-items-center">
-          <div class="col-md-2">
-          <label for="Nombre" class="col-form-label">Nombre de la categoría:</label>
-          </div>
-          <div class="col-md-6">
-          <input type="text" class="form-control" name="Nombre" placeholder="Ingresa el nombre de la categoría" aria-label="Nombre" aria-describedby="basic-addon1">
-          </div>
-          <div class="col-md-3">
-          <select class="form-select" name="Categoria" aria-label="Default select example">
-            <option selected>Selecciona la categoría</option>
-            <option value="Empresa">Empresa</option>
-            <option value="Asunto">Asunto</option>
-          </select>
-          </div>
-          <div class="col-md-1">
-          <button type="submit" class="btn btn-danger">AGREGAR</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="../js/jquery-3.1.1.min.js"></script>
 <script src="../js/bootstrap.bundle.min.js"></script>
