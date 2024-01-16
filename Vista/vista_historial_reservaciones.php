b692e8a9b8b56991fd45deda123f752262f7ac2e
anyt1 <anasesmagarcia@gmail.com>
2024-01-16 11:51:44 -0600

    hfjhfhfhff

--- Vista/vista_historial_reservaciones.php
+++ Vista/vista_historial_reservaciones.php
@@ -1,11 +1,3 @@
-<?php
-$id=$_GET["id"];
-echo $id;
-?>
-<?php
-$mensaje = isset($_GET['mensaje']) ? urldecode($_GET['mensaje']) : "";
-?>
-
 
 <?php
 require_once("../Modelo/conexion2.php");
@@ -28,7 +20,7 @@ $row = $resultado->fetch_assoc();
 </head>
 
 <style>
-.navbar-custom {
+  .navbar-custom {
     background-color: #F73B3B; /* Darle color al NAV, del color que se necesite */
     font-size: 18px; /* Hacer las letras más grandes */
   }
@@ -45,7 +37,7 @@ $row = $resultado->fetch_assoc();
 
   .my-custom-scrollbar {
   position: relative;
-  height: 400px;
+  height: 350px;
   overflow: auto;
   }
 
@@ -56,14 +48,15 @@ $row = $resultado->fetch_assoc();
   .tit-color{
     color:white;
   }
+
 </style>
 
 <header>
-<nav class="navbar navbar-dark fixed-top navbar-custom">
+<nav class="navbar navbar-dark  fixed-top navbar-custom">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img id="logo" src="../imagenes/Logo-Urspace.png" width="95">SRCV SALAS</a>
+    <a class="navbar-brand" href="#"><img id="logo" src="../imagenes/Logo-Urspace.png" width="95">SRCV URSPACE</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end navbar-custom" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
       <div class="offcanvas-header">
@@ -72,16 +65,16 @@ $row = $resultado->fetch_assoc();
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="vista_mapa_salas.php">Mapa</a>
+            <a class="nav-link" aria-current="page" href="vista_mapa_espacios.php">Mapa</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="vista_historial_reservaciones.php">Reservaciones</a>
+            <a class="nav-link" aria-current="page" href="vista_reservaciones_urspace.php">Reservaciones</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="vista_registro_salas.php">Registro de espacios</a>
+            <a class="nav-link" aria-current="page" aria-current="page" href="vista_registro_espacios.php">Registro de espacios</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="vista_reservaciones_canceladas.php">Historial de reservaciones</a>
+            <a class="nav-link active" aria-current="page" href="vista_historial_reservaciones.php">Historial de reservaciones</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" aria-current="page" href="../Controlador/controlador_cerrar_sesion.php">Cerrar Sesión</a>
@@ -89,22 +82,13 @@ $row = $resultado->fetch_assoc();
         </ul>
       </div>
     </div>
-  </div>
+  </div>  
 </nav>
 </header>
-<br>
-<br>
-<br>
+<br><br><br>
+<div class="mb-5"></div> <!--Salto de linea-->
 <h3 class="text-center">HISTORIAL DE RESERVACIONES</h3>
-<div class="mb-4"></div> <!--Salto de linea-->
-<div class="row">
-  <div class="col-md-1"></div>
-  <div class="col-md-10" id="mensaje">
-    <?php echo $mensaje; ?>
-  </div>
-  <div class="col md-1"></div>
-</div>
-<div class="mb-3"></div><!--Salto de linea-->
+<div class="mb-5"></div>
 
 <div class="container">
   <div class="row">
@@ -116,7 +100,7 @@ $row = $resultado->fetch_assoc();
               <th scope="col">Nombre del cliente</th>
               <th scope="col">Apellido paterno</th>
               <th scope="col">Apellido Materno</th>
-              <th scope="col">Espacio</th>
+              <th scope="col">Sala</th>
               <th scope="col">Correo electrónico</th>
               <th scope="col">Fecha de entrada</th>
               <th scope="col">Fecha de salida</th>
@@ -125,13 +109,12 @@ $row = $resultado->fetch_assoc();
               <th scope="col">Total</th>
               <th scope="col">Enganche</th>
               <th scope="col">Liquidación</th>
-              <th scope="col">Acciones</th>
             </tr>
           </thead>
           <?php
               require_once("../Modelo/conexion2.php");
               $conexion = conect();
-              $query = mysqli_query ($conexion, "SELECT * FROM srcv_reservaciones WHERE ESTATUS='1' AND USO='0'");
+              $query = mysqli_query ($conexion, "select * from srcv_reservaciones");
               while($filas  = mysqli_fetch_assoc($query)){
           ?>
           <tr>
@@ -147,92 +130,6 @@ $row = $resultado->fetch_assoc();
               <td><?php echo$filas ["TOTAL"] ?></td>
               <td><?php echo$filas ["ENGANCHE"] ?></td>
               <td><?php echo$filas ["LIQUIDACION"] ?></td>
-              <td>
-                
-
-                  <!-- Modificar reservaciones -->
-                  <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal_<?php echo $filas['ID_RESERVACION'] ?>" onclick="Reservacion('<?php $filas['ID_RESERVACION'] ?>')"> <i class="fa fa-clock-o" aria-hidden="true"></i></a>
-                  <!-- Modal para modificar reservaciones-->
-                  <div class="modal fade" id="exampleModal_<?php echo $filas['ID_RESERVACION'] ?>"  tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h1 class="modal-title fs-5" id="exampleModalLabel">Modificar reservacion de <?= $filas['NOMBRE_CLIENTE'] ?></h1>
-                          <input id="Reservacion_<?php echo $filas['ID_RESERVACION'] ?>" name="Reservacion" value="" hidden>
-                            <input type="hidden" name="idreservacion" id="idreservacion" value="<?php echo $filas['ID_RESERVACION'] ?>">
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="mb-3"></div> <!--Salto de linea-->
-
-                        <div class="modal-body">
-                          <form action="../Controlador/controlador_posponer_reservacion.php" class="formulario row g-3 needs-validation" method="post" novalidate>
-                            <div class="mb-3"></div> <!-- Salto de línea -->
-                              <input type="hidden" name="id" value="<?= $filas['ID_RESERVACION'] ?>">
-                            <div class="row">
-                              <div class="col">
-                                <label for="Fecha inicio">Fecha de inicio *</label>
-                                <input type="date" class="form-control" name="Fechainicio" value="<?=$filas['FECHA_ENTRADA']?>" placeholder="Fecha de inicio" aria-label="Fecha  de inicio" aria-describedby="basic-addon1" required>
-                                <div class="invalid-feedback">
-                                Verifique los datos
-                                </div>
-                              </div>
-                              <div class="col">
-                                <label for="Fecha finalizacion">Fecha de finalizacion *</label>
-                                <input type="date" class="form-control" name="Fechafinalizacion" value="<?=$filas['FECHA_SALIDA']?>" placeholder="Fecha de finalizacion" aria-label="Fecha  de finalizacion" aria-describedby="basic-addon1" required>
-                                <div class="invalid-feedback">
-                                Verifique los datos
-                                </div>
-                              </div>
-                            </div>
-                            <div class="mb-2"></div> <!--Salto de linea-->
-
-                            <div class="row">
-                              <div class="col">
-                                <label for="Hora inicio">Hora de inicio *</label>
-                                <input type="time" class="form-control" name="Horainicio" value="<?=$filas['HORA_ENTRADA']?>" placeholder="Hora de inicio" aria-label="Hora de inicio" aria-describedby="basic-addon1" required>
-                                <div class="invalid-feedback">
-                                Verifique los datos
-                                </div>
-                              </div>
-                              <div class="col">
-                                <label for="Hora finalizacion">Hora de finalización *</label>
-                                <input type="time" class="form-control" name="Horafinalizacion" value="<?=$filas['HORA_SALIDA']?>"placeholder="Hora de finalizacion" aria-label="Hora  de finalizacion" aria-describedby="basic-addon1" required>
-                                <div class="invalid-feedback">
-                                Verifique los datos
-                                </div>   
-                              </div>     
-                            </div>
-                            <div class="mb-3"></div> <!--Salto de linea-->
-
-                          <div class="row">
-                            <div class="col">
-                              <input type="hidden" name="Total" value="<?= $filas['TOTAL'] ?>">
-                            </div>
-                            <div class="col">
-                              <input type="hidden" name="enganche" value="<?= $filas['ENGANCHE'] ?>">
-                              <label for="Abono">Abono *</label>
-                              <input type="number" class="form-control" name="Abono" placeholder="Abono" aria-label="Abono" aria-describedby="basic-addon1" required>
-                              <div class="invalid-feedback">
-                                Verifique los datos
-                              </div>
-                            </div>
-                            <div class="col"><input type="hidden" name="Liquidacion" value="<?= $filas['LIQUIDACION'] ?>"></div>
-                          </div>
-                            <div class="mb-5"></div> <!--Salto de linea-->
-                            <div class="modal-footer">
-                              <button type="submit" class="btn btn-primary">Confirmar</button>
-                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                            </div>
-                          </form>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <!--Boton para eliminar-->
-                  <a href="../Controlador/controlador_eliminar_reservacion.php?id=<?=$filas ['ID_RESERVACION']?>"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
-                  <a href="../Controlador/controlador_uso_reservacion.php?id=<?=$filas['ID_RESERVACION']?>"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
-                
-              </td>
           </tr>
           <?php
           };
@@ -254,18 +151,6 @@ $row = $resultado->fetch_assoc();
 
 <script src="../js/jquery-3.1.1.min.js"></script> <!-- Abra y cierre el menú -->
 <script src="../js/bootstrap.bundle.min.js"></script>
-<script src="../js/validaciones_reservaciones.js"></script>
-
-<script>
-
-function Reservacion(idreservacion){
-    document.getElementById('Reservacion_' + idreservacion).value = idreservacion;
-    <?php $id_reservacion ?>=idreservacion;
-    }
-
-
-
-</script>
 
 </body>
 </html>
