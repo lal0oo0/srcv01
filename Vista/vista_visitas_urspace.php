9d884d010a9242937e16e771dec5634e4502654a
lal0oo0 <clalo2341@gmail.com>
2024-03-12 14:12:54 -0600

    nueva vista

---
+++ Vista/vista_visitas_urspace.php
@@ -0,0 +1,210 @@
+<?php
+require_once("../Modelo/conexion2.php");
+$conexion = conect();
+session_start();
+if (empty($_SESSION["correo"])){
+  header("location: vista_inicio_sesion.php");
+}
+$correo = $_SESSION["correo"];
+$sql  = "SELECT CORREO_ELECTRONICO, NOMBRE FROM srcv_administradores WHERE CORREO_ELECTRONICO = '$correo' ";
+$resultado = $conexion->query($sql);
+$row = $resultado->fetch_assoc();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/font-awesome-4.7.0/css/font-awesome.min.css">
+    <title>Reservaciones</title>
+</head>
+
+<style>
+  .navbar-custom {
+    background-color: #F73B3B; /* Darle color al NAV, del color que se necesite */
+    font-size: 18px; /* Hacer las letras más grandes */
+  }
+
+  thead{/*EStilos para la cabecera fija de la tabla*/
+    position: sticky;
+    top:0;
+  }
+
+  table.table th,
+  table.table td {
+    text-align: center;
+  }
+
+  .my-custom-scrollbar {
+  position: relative;
+  height: 350px;
+  overflow: auto;
+  }
+
+  .table-wrapper-scroll-y {
+  display: block;
+  }
+
+  .tit-color{
+    color:white;
+  }
+
+  .filtro{
+    display: none;
+  }
+
+</style>
+
+<header>
+<nav class="navbar navbar-dark  fixed-top navbar-custom">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img id="logo" src="../imagenes/Logo-Urspace.png" width="95">SRCV URSPACE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end navbar-custom" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h3 class="offcanvas-title tit-color" id="offcanvasDarkNavbarLabel"> Bienvenid@ <?php echo utf8_decode($row['NOMBRE']); ?> </h3>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="vista_mapa_espacios.php">Mapa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="vista_reservaciones_urspace.php">Reservaciones</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" aria-current="page" href="vista_registro_espacios.php">Registro de espacios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="vista_historial_reservaciones.php">Historial de reservaciones</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="../Controlador/controlador_cerrar_sesion.php" onclick="cerrarsesion(event)">Cerrar Sesión</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>  
+</nav>
+</header>
+<br><br><br>
+<div class="mb-5"></div> <!--Salto de linea-->
+<h3 class="text-center">VISITAS</h3>
+<div class="mb-5"></div>
+
+<div class="container">
+
+  <!--Bucador-->
+  <div class="row">
+    <div class="col-md-9"></div>
+    <div class="col-md-3">
+      <input class="form-control mr-sm-2" type="search" id="buscador" name="buscador" placeholder="Buscar" aria-label="Search" style="border: 1px solid rgba(0, 0, 0, 0.7);">
+    </div>
+  </div>
+  <div class="mb-3"></div><!--Salto de linea-->
+
+  <div class="row">
+    <div class="col">
+      <div class="table-responsive my-custom-scrollbar">
+        <table class="table table-bordered table-striped mb-0">
+          <thead class="table-dark">
+            <tr>
+            <th scope="col">Hora de entrada</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellido Paterno</th>
+            <th scope="col">Apellido Materno</th>
+            <th scope="col">Asunto</th>
+            <th scope="col">Hora de salida</th>
+            </tr>
+          </thead>
+          <?php
+              require_once("../Modelo/conexion2.php");
+              $conexion = conect();
+              $query = mysqli_query ($conexion, "SELECT * FROM srcv_visitas WHERE EMPRESA='UrSpace' AND ASUNTO != 'Reservacion'");
+              while($filas  = mysqli_fetch_assoc($query)){
+          ?>
+          <tr class="datos">
+                    <td><?php echo $filas['ENTRADA_SEGURIDAD'] ?></td>
+                    <td><?php echo $filas['FECHA'] ?></td>
+                    <td><?php echo $filas['NOMBRE'] ?></td>
+                    <td><?php echo $filas['APELLIDO_PATERNO'] ?></td>
+                    <td><?php echo $filas['APELLIDO_MATERNO'] ?></td>
+                    <td><?php echo $filas['ASUNTO'] ?></td>
+                    <td><?php echo $filas['SALIDA_SEGURIDAD'] ?></td>
+          </tr>
+          <?php
+          };
+          ?>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="mb-3"></div> 
+<div class="row">
+  <div class="col-md-10">
+  </div>
+  <div class="col-md-2">
+  <a class="btn btn-dark" class="tit-color" style="background-color:#008000" href="../PhpSpreadsheet/reporte_urspace.php"><img src="../imagenes/excel.png" width="40px">Informe</a>
+</div>
+</div>
+
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script><!--sweetalert sea local-->
+<script src="../js/jquery-3.1.1.min.js"></script> <!-- Abra y cierre el menú -->
+<script src="../js/bootstrap.bundle.min.js"></script>
+
+<script>
+  //Script de buscador
+  document.addEventListener('keyup', e =>{
+    if(e.target.matches('#buscador')){
+      document.querySelectorAll('.datos').forEach(dato =>{
+        dato.textContent.toLowerCase().includes(e.target.value)
+        ? dato.classList.remove('filtro')
+        : dato.classList.add('filtro')
+      }) 
+    }
+  })
+  //fin del script de buscardor
+</script>
+
+
+<!--script para mostrar alerta de confirmación antes de cerrar sesión-->
+<script>
+  function cerrarsesion(event) {
+    // Previene el comportamiento predeterminado del enlace
+    event.preventDefault();
+
+    // Muestra la alerta de SweetAlert
+    swal("¿Estás seguro de que deseas cerrar sesión?", {
+      buttons: ["Cancelar", "Aceptar"],
+    }).then(function (confirmed) {
+      // confirmed será true si se hace clic en "Aceptar", false si se hace clic en "Cancelar"
+      if (confirmed) {
+        // Realiza una solicitud Ajax al servidor para cerrar sesión
+        $.ajax({
+          type: "POST",
+          url: "../Controlador/controlador_cerrar_sesion.php",
+          //data: { key1: 'value1', key2: 'value2' },
+          dataType: "json",
+          success: function(response) {
+            if (response.success) {
+                // Redirige a otra interfaz después de cerrar la alerta (opcional)*/
+                window.location.href = "../Vista/vista_inicio_sesion.php";
+            } else {
+              // Muestra una alerta de error con SweetAlert
+              swal('Error', response.error, 'error');
+            }
+          }
+        });
+      }
+    });
+  }
+</script>
+</body>
+</html>
