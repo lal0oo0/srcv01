d11a6b79e3a5612609091296be6dfbf456935db3
lal0oo0 <clalo2341@gmail.com>
2024-03-14 14:22:16 -0600

    motivos

---
+++ Controlador/controlador_motivo.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+$usermodi= $_SESSION['correo'];
+
+/*Codigo de conexion a la base de datos*/
+include '../Modelo/conexion2.php';
+/* Obtener la conexión a la base de datos */
+$conexion = conect();
+
+
+// Obtén la fecha y hora actual
+date_default_timezone_set('America/Mexico_City');
+$fechamodificacion = date('Y-m-d H:i:s');
+$motivo= $_POST['motivo'];
+$id= $_POST['id'];
+
+
+  $sql = "UPDATE srcv_visitas SET MOTIVO='$motivo', USUARIO_MODIFICACION='$usermodi', FECHA_MODIFICACION='$fechamodificacion' where ID_VISITA='$id'";
+  $resultado=mysqli_query($conexion,$sql);
+
+/*cambiar alertas*/
+    if ($resultado) {
+    // Éxito: alerta de Bootstrap éxito
+    $mensaje = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Exito!</strong> El registro se ha actualizado correctamente
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>';
+    } else {
+    // Error: alerta de Bootstrap error con detalles
+    $mensaje = '<div class="alert alert-danger" role="alert">Error al actualizar su registro' . mysqli_error($conexion) . '</div>';
+    }
+mysqli_close($conexion);
+
+header("location: ../Vista/vista_visitas_urspace.php?mensaje=" . urlencode($mensaje));
+
+?>
