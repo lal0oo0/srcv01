b2fbd5d05c66ad2e5639f57431ab0744c3606102
anyt1 <anasesmagarcia@gmail.com>
2024-04-17 10:21:58 -0600

    lineas para verificar errores

--- Controlador/controlador_registrar_usu.php
+++ Controlador/controlador_registrar_usu.php
@@ -1,4 +1,8 @@
 <?php
+// Habilitar la visualización de errores
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 //Codigo de conexion a la base de datos
 include '../Modelo/conexion2.php';
