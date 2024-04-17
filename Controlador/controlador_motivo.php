001bbb2e3e8906a3504f55c69169c072405ac81c
anyt1 <anasesmagarcia@gmail.com>
2024-04-17 10:50:20 -0600

    verificacion para visualizar errores

--- Controlador/controlador_motivo.php
+++ Controlador/controlador_motivo.php
@@ -1,4 +1,9 @@
 <?php
+// Habilitar la visualización de errores
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 $usermodi= $_SESSION['correo'];
 
